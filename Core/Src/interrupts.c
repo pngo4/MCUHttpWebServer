95fb750c3f44b3f9f86f2611646428a2cef7bbfe
pngo4 <duc200444@gmail.com>
2025-11-10 22:30:57 -0800

    added w5500 drivers
    Still need to change wizport.c. Specifically need to change SPI handle and GPIOs for CS and RST

---
+++ Core/Src/interrupts.c
@@ -0,0 +1,27 @@
+/**
+ * @file 	interrupts.c
+ * @brief 	GPIO interrupt handler implementation example
+ * @note 	Platform and system dependent 
+ * @date 	8 feb 2020 г.
+ * @author 	Peter Borisenko
+ */
+
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
+extern SemaphoreHandle_t s_xSemaphoreSpi;
+
+void EXTI_IRQ9_5Handler(void) {
+  portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+  if ((EXTI->IMR & EXTI_Line6) && (EXTI->PR & EXTI_Line6)) {
+    EXTI_ClearITPendingBit(0x007FFFFF); // Clear all flags
+    xSemaphoreGiveFromISR(s_xSemaphoreSpi, &xHigherPriorityTaskWoken);
+  }
+  // Switch tasks if necessary
+  if (xHigherPriorityTaskWoken != pdFALSE) {
+    portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
+  }
+}
+
